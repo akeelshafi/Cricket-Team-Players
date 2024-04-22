fd9c418ef328ad921ad01e8062054f100f26d179
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-04-22 21:08:23 +0530

    third day commit

--- app/src/main/java/com/twoplus/secondbasictestapplication/NewActivity.kt
+++ app/src/main/java/com/twoplus/secondbasictestapplication/NewActivity.kt
@@ -1,11 +1,45 @@
 package com.twoplus.secondbasictestapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
-class NewActivity : AppCompatActivity() {
+class NewActivity : AppCompatActivity(){
+
+    private lateinit var editMobileNumber:EditText
+    private lateinit var editPassword:EditText
+    private lateinit var editLoginBtn:Button
+    private lateinit var editForgot:TextView
+    private val validMobileNumber ="6005027963"
+    private val validPassword ="akeel"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+
+        editMobileNumber = findViewById(R.id.phoneNumber)
+        editPassword = findViewById(R.id.password)
+        editLoginBtn = findViewById(R.id.LoginBtn)
+        editForgot = findViewById(R.id.forgotPassword)
+        editLoginBtn.setOnClickListener {
+
+            val mobileNumber = editMobileNumber.text.toString()
+            val password = editPassword.text.toString()
+
+            if (mobileNumber==validMobileNumber && password==validPassword){
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this,"Incorrect Number or Password",Toast.LENGTH_LONG).show()
+            }
+
+            Toast.makeText(this,"Welcome to pakistan cricket team",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
